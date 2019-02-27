b714a03585ca2bc40212f350a5f6061db44ebc5c
Bijaya Manandhar <bijaya.manandhar@yahoo.com>
2019-02-27 15:38:45 -0800

    adds all changes

--- script.js
+++ script.js
@@ -4,30 +4,32 @@ $(document).ready(function(){
     col='ABCDEFGH'.split('');
     row='12345678'.split('');
     
-    men_white=['men/wP.png','men/wR.png','men/wN.png','men/wB.png','men/wK.png','men/wQ.png','men/wB.png','men/wN.png','men/wR.png'];
-    men_black=['men/bP.png','men/bR.png','men/bN.png','men/bB.png','men/bK.png','men/bQ.png','men/bB.png','men/bN.png','men/bR.png'];
+    men_white=['men/wR.png','men/wN.png','men/wB.png','men/wK.png','men/wQ.png','men/wB.png','men/wN.png','men/wR.png'];
+    pond_white='men/wP.png';
+    men_black=['men/bR.png','men/bN.png','men/bB.png','men/bK.png','men/bQ.png','men/bB.png','men/bN.png','men/bR.png'];
+    pond_black='men/bP.png';
 
     for (let i=0; i<=7; i++){
-      $('.white-row-zero').append(`<img src="${men_white[0]}" style="width:35px; height:35px;">`);
-      $('.black-row-zero').append(`<img src="${men_black[0]}" style="width:35px; height:35px;">`);
+      $('.white-row-one').append(`<img src="${pond_white}" style="width:35px; height:35px;">`);
+      $('.black-row-one').append(`<img src="${pond_black}" style="width:35px; height:35px;">`);
     }
 
-    for (let i=1; i<=8; i++){
-      $('.white-row-one').append(`<img src="${men_white[i]}" style="width:35px; height:35px;">`);
-      $('.black-row-one').append(`<img src="${men_black[i]}" style="width:35px; height:35px;">`);
+    for (let i=0; i<=7; i++){
+      $('.white-row-two').append(`<img src="${men_white[i]}" style="width:35px; height:35px;">`);
+      $('.black-row-two').append(`<img src="${men_black[i]}" style="width:35px; height:35px;">`);
     }
     for (let i=7; i>=0; i--){
       for (let j=0; j<=7; j++){
         if ((i+j)%2===0){
         $(`.row-${i+1}`).append(
           `<div class="black-square">
-            <p>${col[j]}${row[i]}</p>
+            <p class="${col[j]}${row[i]}">${col[j]}${row[i]}</p>
           </div>
       `)
       } else {
         $(`.row-${i+1}`).append(
           `<div class="white-square">
-            <p>${col[j]}${row[i]}</p>
+            <p class="${col[j]}${row[i]}">${col[j]}${row[i]}</p>
           </div>
       `)
 
@@ -35,26 +37,9 @@ $(document).ready(function(){
     }
   }
 
-
-
-    
-  // for (let i=col.length-1; i>=0; i--){
-  //   for (let j=row.length-1; j>=0; j--){
-  //     if ((i+j)%2===0){
-  //       $(`.row-${i}`).append(
-  //        `<div class="white-square">
-  //           <div class="${col[i]}${row[j]}">
-  //             <p>${col[i]}${row[j]}</p>
-  //           </div>
-  //         </div>`)
-  //     } else {
-  //     $(`.row-${i}`).append(
-  //       `<div class="black-square">
-  //         <div class="${col[i]}${row[j]}">
-  //           <p>${col[i]}${row[j]}</p>
-  //         </div>
-  //       </div>`)        
-  //     }
+  // $('.start').click(function(){
+  //   for (let i=0; i<=7; i++){
+  //     $('.${col[i]}${i}').html(`<img src="">`)
   //   }
-  // }  
+  // } 
 });
